fefa96541c2bb87453682c6c551c1513471d5bcd
vmshaneeb <vmshaneeb@gmail.com>
2020-08-21 00:36:35 +0530

    Swift PM manifest fix 5

--- Package.swift
+++ Package.swift
@@ -13,19 +13,18 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-//        .package(url: "https://github.com/vmshaneeb/FGRoute.git", from: "1.0.5")
+        //        .package(url: "https://github.com/vmshaneeb/FGRoute.git", from: "1.0.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FGRoute",
-//            path: "FGRoute"
-                        dependencies: []),
-            //        .testTarget(
-            //            name: "FGRouteTests",
-            //            dependencies: ["FGRoute"]
-//        ),
+            dependencies: [], path: "Sources"),
+        //        .testTarget(
+        //            name: "FGRouteTests",
+        //            dependencies: ["FGRoute"]
+        //        ),
     ],
     swiftLanguageVersions: [.v5]
 )
