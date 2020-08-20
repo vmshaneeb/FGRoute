71a2eb3a83e105a00af04f2a28b1fb96abb0c715
vmshaneeb <vmshaneeb@gmail.com>
2020-08-20 19:43:34 +0530

    Swift PM manifest fix 2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/vmshaneeb/FGRoute.git", from: "1.0.5")
     ],
     targets: [
@@ -21,9 +20,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FGRoute",
-            dependencies: []),
-        .testTarget(
-            name: "FGRouteTests",
-            dependencies: ["FGRoute"]),
-    ]
+            path: "FGRoute"
+            //            dependencies: []),
+            //        .testTarget(
+            //            name: "FGRouteTests",
+            //            dependencies: ["FGRoute"]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )
